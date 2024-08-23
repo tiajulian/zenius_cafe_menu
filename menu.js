fdf665337d8be53f9827ec36a6f29e7568d43a4e
Tia Julian <80685888+tiajulian@users.noreply.github.com>
2024-08-23 12:54:57 +1000

    Update menu.js

--- menu.js
+++ menu.js
@@ -42,23 +42,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
         .catch(error => console.error('Error fetching the menu:', error));
-
-    // Show the pop-up message on page load
-    const popup = document.getElementById('popup-message');
-    const closeBtn = document.querySelector('.close-btn');
-
-    // Show the popup
-    popup.classList.add('show');
-
-    closeBtn.onclick = function() {
-        popup.classList.remove('show');
-    }
-
-    window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.classList.remove('show');
-        }
-    }
 });
 
 function capitalizeFirstLetter(string) {
