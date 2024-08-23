1ae4b97fc9ca622f348dab8bec0e639b21eba4cb
Tia Julian <80685888+tiajulian@users.noreply.github.com>
2024-08-23 14:17:14 +1000

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const closeBtn = document.querySelector('.close-btn');
     const body = document.body;
 
-    // Function to close the popup
     function closePopup() {
         popup.classList.remove('show');
         body.classList.remove('body-popup-active');
@@ -13,14 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     popup.classList.add('show');
     body.classList.add('body-popup-active');
 
-    // Close the popup when close button is clicked
     if (closeBtn) {
         closeBtn.onclick = closePopup;
     } else {
         console.error('Close button not found');
     }
 
-    // Close the popup when clicking outside of it
     window.onclick = function(event) {
         if (event.target === popup) {
             closePopup();
